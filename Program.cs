9e30c6ecb7349a2438850dc9d64bc3ec3700717a
Thael Matos <thael28@hotmail.com>
2020-02-02 00:08:43 -0400

    centrado calculadora basica

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace Calculadora1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string texto_a_centrar = "Calculadora Basica";
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (texto_a_centrar.Length / 2)) + "}", texto_a_centrar));
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            Console.ReadKey();
         }
     }
 }
